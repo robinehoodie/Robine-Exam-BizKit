b5653d29d769cc0de581dc75b10895e9341903f9
@17100454 <robinecole260@gmail.com>
2023-09-08 21:26:55 +0800

    Finished answering match and search assessment

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,38 @@ def search_users(args):
     """
 
     # Implement search here!
+    if args:
+        ret = []
+        if len(args) > 0:
+            for key, value in args.items():
+                if key == "id":
+                    if int(value) <= len(USERS):
+                        insert(USERS[int(value)-1], ret)
+                        
+                elif key == "name":
+                    temp = [k for k in USERS if value.lower() in k["name"].lower()]
+                    insert(temp, ret)
+                        
+                elif key == "age":
+                    temp = [k for k in USERS if int(k["age"]) in range(int(value)-1, int(value)+2)]
+                    insert(temp, ret)
 
-    return USERS
+                elif key == "occupation":
+                    temp = [k for k in USERS if value.lower() in k["occupation"].lower()]
+                    insert(temp, ret)
+
+        return ret
+    else:
+        return USERS
+    
+def is_exists(user, ret):
+     return bool([k["id"] for k in ret if k["id"] == user["id"]]) 
+
+def insert(temp, ret):
+    if type(temp)== list:
+        for x in temp:
+            if not is_exists(x, ret):
+                ret.append(x)
+    else:
+        if not is_exists(temp, ret):
+            ret.append(temp)
